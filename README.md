d0eaeace2b0cec1f040ab1fdea58c055cbddc923
haldarsaurav <59361412+haldarsaurav@users.noreply.github.com>
2022-04-21 14:12:33 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 100_Days_of_Code
 All my codes and notes from the course. 
+
--- day_01/random_name_generator.py
+++ day_01/random_name_generator.py
@@ -8,7 +8,7 @@ while True:
     print("What is your best friends name?")
     friends_name = input("> ")
     if friends_name == "":
-        print("You have not entered anything, please write your friends name agian.")
+        print("You have not entered anything, please write your friends name agian.\n")
     else:
         break
     
@@ -16,7 +16,7 @@ while True:
     print("What is your pet's name?")
     pets_name = input("> ") 
     if pets_name == "":
-        print("You have not entered anything, please enter your pets name again.")
+        print("You have not entered anything, please enter your pets name again.\n")
     else:
         break
     
@@ -25,4 +25,4 @@ while True:
 fn = friends_name.title()
 pn = pets_name.title()
 
-print(f"Your name cane be = {fn}_{pn}")
+print(f"Your name cane be = {fn} {pn}\n")
